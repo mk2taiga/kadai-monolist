c2d7c5938b82a3737032db2b09e2cd49821773c6
Taiga Miyazaki <mk2_tg000@icloud.com>
2017-06-08 13:01:18 +0000

    registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::get('/', 'WelcomeController@index');
+
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
